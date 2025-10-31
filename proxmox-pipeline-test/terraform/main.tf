dc891fb130170dd634b234e92ed0ed728525b691
Dimitar Porkov <56470141+borntoeatt@users.noreply.github.com>
2025-10-31 17:59:23 +0200

    Update main.tf

--- proxmox-pipeline-test/terraform/main.tf
+++ proxmox-pipeline-test/terraform/main.tf
@@ -1,21 +1,31 @@
 resource "proxmox_virtual_environment_container" "testbox" {
   node_name = "pve"
   vm_id     = 20001
-  clone { vm_id = 9001 }
+  clone {
+    vm_id = 9001
+  }
   description = "Test container"
-  cpu { cores = 1 }
-  memory { dedicated = 512 }
+  cpu {
+    cores = 1
+  }
+  memory {
+    dedicated = 512
+  }
   network_interface {
     name   = "eth0"
     bridge = "vmbr0"
   }
-  features { nesting = true }
+  features {
+    nesting = true
+  }
   initialization {
     hostname = "testbox"
     ip_config {
-      ipv4 { address = "dhcp" }
+      ipv4 {
+        address = "dhcp"
+      }
     }
   }
   start_on_boot = false
   started       = true
-
+}
