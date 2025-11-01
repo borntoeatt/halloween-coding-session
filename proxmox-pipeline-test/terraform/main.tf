bd1ea3e74677e0c105651aa474e72cc273011ebe
Dimitar Porkov <56470141+borntoeatt@users.noreply.github.com>
2025-11-01 21:31:11 +0200

    adding nextcloud

--- proxmox-pipeline-test/terraform/main.tf
+++ proxmox-pipeline-test/terraform/main.tf
@@ -35,3 +35,38 @@ resource "proxmox_virtual_environment_container" "testbox" {
   start_on_boot = false
   started       = true
 }
+
+
+# Nextcloud container
+esource "proxmox_virtual_environment_container" "nextcloud" {
+  depends_on = [proxmox_virtual_environment_container.testbox]
+  node_name = "pve"
+  vm_id     = 20009
+  clone {
+    vm_id = 9001
+  }
+  description = "Test nextcloud container"
+  cpu {
+    cores = 1
+  }
+  memory {
+    dedicated = 512
+  }
+  network_interface {
+    name   = "eth0"
+    bridge = "vmbr0"
+  }
+  features {
+    nesting = true
+  }
+  initialization {
+    hostname = "nextcloud"
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+  start_on_boot = false
+  started       = true
+}
